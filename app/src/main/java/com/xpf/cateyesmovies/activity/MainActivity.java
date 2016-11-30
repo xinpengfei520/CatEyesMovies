ff47038912add60d0aa304e2d27a42e0311b6756
xinpengfei520 <542270559@qq.com>
2016-11-30 15:41:04 +0800

    complete framework

--- app/src/main/java/com/xpf/cateyesmovies/activity/MainActivity.java
+++ app/src/main/java/com/xpf/cateyesmovies/activity/MainActivity.java
@@ -1,15 +1,171 @@
 package com.xpf.cateyesmovies.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.xpf.cateyesmovies.R;
+import com.xpf.cateyesmovies.common.BaseFragment;
+import com.xpf.cateyesmovies.fragment.DescoverFragment;
+import com.xpf.cateyesmovies.fragment.MovieFragment;
+import com.xpf.cateyesmovies.fragment.MovieTheatreFragment;
+import com.xpf.cateyesmovies.fragment.MyFragment;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends FragmentActivity {
+
+    private static final int MESSAGE_BACK = 1;
+    @BindView(R.id.tv_select_city)
+    TextView tvSelectCity;
+    @BindView(R.id.ll_search)
+    LinearLayout llSearch;
+    @BindView(R.id.fl_container)
+    FrameLayout flContainer;
+    @BindView(R.id.rb_movie)
+    RadioButton rbMovie;
+    @BindView(R.id.rb_theatre)
+    RadioButton rbTheatre;
+    @BindView(R.id.rb_descover)
+    RadioButton rbDescover;
+    @BindView(R.id.rb_my)
+    RadioButton rbMy;
+    @BindView(R.id.rg_main)
+    RadioGroup rgMain;
+
+    private List<BaseFragment> fragments;
+    private int currentPosition = 0; // 默认为位置为0
+    private Fragment tempFragment; // 用于存储临时的Fragment
+
+    private boolean isFlag = true;
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_BACK:
+                    isFlag = true; // 在2s时,恢复isFlag的变量值
+                    break;
+            }
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initFragment();
+        initListener();
+
+        rgMain.check(R.id.rb_movie); // 默认选中电影页面
+    }
+
+    private void initFragment() {
+        fragments = new ArrayList<>();
+        fragments.add(new MovieFragment());
+        fragments.add(new MovieTheatreFragment());
+        fragments.add(new DescoverFragment());
+        fragments.add(new MyFragment());
+    }
+
+    private void initListener() {
+        rgMain.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.rb_movie:
+                        currentPosition = 0;
+                        break;
+                    case R.id.rb_theatre:
+                        currentPosition = 1;
+                        break;
+                    case R.id.rb_descover:
+                        currentPosition = 2;
+                        break;
+                    case R.id.rb_my:
+                        currentPosition = 3;
+                        break;
+                }
+                BaseFragment fragment = getFragmentByPosition(currentPosition);
+                switchFragment(tempFragment, fragment);
+            }
+        });
+    }
+
+    private void switchFragment(Fragment fromFragment, Fragment toFragment) {
+
+        if (tempFragment != toFragment) {
+            tempFragment = toFragment;
+            if (toFragment != null) {
+                // 开启事务
+                FragmentTransaction transaction = this.getSupportFragmentManager().beginTransaction();
+
+                // 判断要显示的Fragment是否已经被添加
+                if (!toFragment.isAdded()) {
+                    if (fromFragment != null) {
+                        transaction.hide(fromFragment); // 隐藏当前的
+                    }
+                    transaction.add(R.id.fl_container, toFragment).commit();// 添加新的
+                } else {
+                    if (fromFragment != null) {
+                        transaction.hide(fromFragment);
+                    }
+                    transaction.show(toFragment).commit();
+                }
+            }
+        }
+    }
+
+    private BaseFragment getFragmentByPosition(int position) {
+        if (fragments != null && fragments.size() > 0) {
+            return fragments.get(position);
+        }
+        return null;
+    }
+
+    @OnClick({R.id.tv_select_city, R.id.ll_search})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_select_city: // 城市快速搜索
+                break;
+            case R.id.ll_search: // 电影搜索
+                break;
+        }
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK && isFlag) {
+            isFlag = false;
+            Toast.makeText(MainActivity.this, "再次点击返回键退出应用", Toast.LENGTH_SHORT).show();
+            handler.sendEmptyMessageDelayed(MESSAGE_BACK, 2000);
+            return true;
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 保证在activity退出前,移除所有未被执行的消息和回调方法,避免出现内存泄漏!
+        handler.removeCallbacksAndMessages(null);
     }
 }
--- app/src/main/java/com/xpf/cateyesmovies/activity/WelcomeActivity.java
+++ app/src/main/java/com/xpf/cateyesmovies/activity/WelcomeActivity.java
@@ -1,23 +1,36 @@
 package com.xpf.cateyesmovies.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
+import com.bumptech.glide.Glide;
 import com.xpf.cateyesmovies.R;
+import com.xpf.cateyesmovies.utils.AppNetConfig;
 
 // 欢迎页面
 public class WelcomeActivity extends Activity {
 
-    private RelativeLayout rl_welcome;
     private ImageView iv_welcome;
 
     private Handler handler = new Handler() {
         public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    if (isFinishing()) { // 当前Activity已经销毁
+                        return;
+                    }
+                    initWelcomeView(); // 加载图片替换默认图片
+                    break;
 
+                case 1: // 并跳转到主页面,结束当前页面
+                    startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+                    finish();
+                    break;
+            }
         }
     };
 
@@ -26,7 +39,21 @@ public class WelcomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        rl_welcome = (RelativeLayout) findViewById(R.id.rl_welcome);
         iv_welcome = (ImageView) findViewById(R.id.iv_welcome);
+        // 发送一个延时2s的消息
+        handler.sendEmptyMessageDelayed(0, 2000);
+    }
+
+    private void initWelcomeView() {
+        Glide.with(this).load(AppNetConfig.WELCOMEPICURL).into(iv_welcome);
+        handler.sendEmptyMessageDelayed(1, 3000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (handler != null) {
+            handler.removeCallbacksAndMessages(null);
+        }
     }
 }
